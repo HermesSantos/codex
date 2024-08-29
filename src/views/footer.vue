261e062040c176dc903c8100068d778ca3f51d35
HermesSantos <hermessantos995@gmail.com>
2024-08-29 00:53:12 -0300

    adding footer, nav and an initial sidebar

---
+++ src/views/footer.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="container">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <p class="col-md-4 mb-0 text-body-secondary">&copy; 2024 Codex</p>
+
+      <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+      </a>
+
+      <ul class="nav col-md-4 justify-content-end">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
+      </ul>
+    </footer>
+  </div>
+</template>
+<style scoped>
+*{
+  color: #fff !important;
+}
+footer{
+  color: #fff !important;
+  margin-bottom: 0rem !important;
+}
+</style>
---
+++ src/views/nav.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/">Codex</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+</nav>
+</template>
+<style scoped>
+nav{
+  background: #1f1f1f !important;
+}
+a{
+  color: #fff !important
+}
+</style>
